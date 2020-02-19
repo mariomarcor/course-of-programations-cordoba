bb3e2d21fd17eada85058c3dcb07ada7e66ec93b
mariomarcor <mariomarcor3@gmail.com>
2020-02-19 12:06:56 +0100

    proyecto principal

---
+++ 2/2/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// EL INT.PARSE ES UN CONVERSOR
+            //Console.WriteLine("escribe un numero ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("ESCRIBA UN SEGUNDO NUMERO ");
+            //int y = int.Parse(Console.ReadLine());
+            //if (x == y)
+            //{
+            //    Console.WriteLine("SON IGUALES");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no son iguales");
+            //}
+
+
+            //// que te diga si es mayor que otro///
+            //Console.WriteLine("escribe un numero ");
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("ESCRIBA UN SEGUNDO NUMERO ");
+            //int y = int.Parse(Console.ReadLine());
+
+            //if (x == y)
+            //{
+            //    Console.WriteLine("SON IGUALES");
+
+            //}
+            //else if (x > y)
+            //{
+            //    Console.WriteLine("es mayor");
+            //}
+            //else if (x < y)
+            //{
+            //    Console.WriteLine("es menor");
+            //}
+            //// si es par o no es par///
+            //Console.WriteLine("escribe un numero ");
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("ESCRIBA UN SEGUNDO NUMERO ");
+
+            //int y = int.Parse(Console.ReadLine());
+
+            //if ( x % y == 0)
+            //{
+            //    Console.WriteLine("SON par");
+            //}
+            //else if (x % y >= 0)
+
+            //{
+            //    Console.WriteLine("no son par");
+            //}
+
+            ///si es multiplo //
+            //Console.WriteLine("escribe un numero ");
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("ESCRIBA UN SEGUNDO NUMERO ");
+
+            //int y = int.Parse(Console.ReadLine());
+
+            //if (x % y == 0)
+            //{
+            //    Console.WriteLine("SON multiplo");
+            //}
+            //else 
+
+            //{
+            //    Console.WriteLine("no son multiplo");
+            //}
+
+
+            //si es 0  o negativo o positivo//
+            //Console.WriteLine("escribe un numero ");
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("ESCRIBA UN SEGUNDO NUMERO ");
+
+            //int y = int.Parse(Console.ReadLine());
+
+            //if (x - y == 0)
+            //{
+            //    Console.WriteLine("es 0");
+            //}
+            //else if ( x + y == +0)
+            //{
+            //    Console.WriteLine("es positivo");
+            //}
+            //else 
+            //    Console.WriteLine("es negativo");
+
+            /// segun lo que introduzca aparecen diferentes frases///
+            //Console.WriteLine("1. refran ");
+            //Console.WriteLine("2. chiste ");
+            //Console.WriteLine("3. insulto ");
+
+            //Console.WriteLine("escribe un numero ");
+
+            //int x = int.Parse(Console.ReadLine());
+
+
+            //switch (x)
+            //{
+            //    case 1:
+            //        Console.WriteLine("es refran");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("es chiste");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("es insulto");
+            //        break;
+            //    default:
+            //        Console.WriteLine("no es valido");
+
+            //        break;
+
+
+            //hacer nota media//
+
+            //Console.WriteLine("calcula la nota media de tus notas");
+            //Console.WriteLine("mete una nota");
+
+            //     float x = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("mete una nota");
+
+            //     float xy = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("mete una nota");
+
+            //    float xz = float.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("su una nota");
+
+            //    float x_nota = ((x + xy + xz)/3);
+
+            //Console.WriteLine(x_nota);
+
+            //introducir una altura y decir si es alto o bajo///
+            Console.WriteLine("escribe una altura ");
+
+            int xalt = int.Parse(Console.ReadLine());
+
+
+            if (xalt <= 220 && xalt >= 170)
+            {
+                Console.WriteLine("eres altisimo ");
+            }
+
+            else if (xalt <= 170 && xalt > 150)
+            {
+                Console.WriteLine("eres enano ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+}
+
+            
+            
+
+
+
+
+    }
+
+
